7edaba76aea2b938df1cdd8e7193e361b272d6b8
krisssy023 <alaparkristian@gmail.com>
2022-08-20 13:30:44 +0800

    -added rules

--- color-game.js
+++ color-game.js
@@ -1,12 +1,32 @@
-const colors = ["BLUE", "VIOLET", "BLACK","GRAY", "GREEN", "RED","YELLOW","ORANGE","PINK"]
-let human ;
-let winningColor = document.getElementById("winning-color")
 
-function botPick(){
-    let  i = Math.floor(Math.random()*9);
-    let botColor = colors[i] ;
-    winningColor.innerText = `Winning Color : ${botColor} !`
-   
+const colors = document.getElementById("color-container").childNodes
+const winningColorPicker = document.getElementById("color-box")
+let colorArray = []
+
+// const hoverRed = document.getElementById("red").addEventListener(click,pickedByClient)
+
+// function pickedByClient(){
+//    let fontSize = document.getElementById("red").style.fontSize;
+//    fontSize = 20;
+// }
+
+function getColors(){
+    for (let i = 0; i < colors.length;i++){
+        let colorId = colors[i].id;
+        if (typeof(colorId) !== "undefined"){
+            colorArray.push(colorId)
+        }
+    }
 }
+getColors();
 
-console.log(botPick())
+
+function colorBoxes(){
+    for (let i = 0 ; i < colorArray.length; i++){
+       let box = document.getElementById(colorArray[i]);
+       box.style.backgroundColor = colorArray[i];
+    }
+}
+colorBoxes()
+
+
